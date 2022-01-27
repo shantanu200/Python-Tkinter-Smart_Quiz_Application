cba9f4c2e1fd60933b8f0704f58b65aae41d0965
Shantanu Bhusari <shantanubhusari16@gmail.com>
2022-01-27 10:38:53 +0530

    test.py changes

--- test.py
+++ test.py
@@ -6,63 +6,63 @@ from tkinter import *
 root = Tk()
 root.geometry("400x400")
 root.title("TEST_APP")
-
+opt_selected = IntVar()
+opt_selected.set(0)
 db_questions = need_question()
 db_options = need_options()
 db_answers = need_answer()
-rb = tkinter.IntVar()
-rb.set(None)
 score = 0
 num = 0
 
 def action_next():
-    global db_answers,db_options,db_questions,num,rb
-    fun_num = num
-    fun_que = db_questions
-    fun_options = db_options
-    fun_answer = db_answers
-    if(fun_num<len(fun_que)):
-        fun_num += 1
-        num = fun_num
-        label.config(text=fun_que[fun_num])
-        rb.set(0)
-        get_options(fun_num)
-        check_ans(fun_num)
-    else:
-        print("quiz is over")
+    global num
+    num += 1
+    get_questions(num)
+    get_options(num)
+    check_ans(num)
         
 #we need options to fetch:
+
+def get_questions(que_num):
+    global db_questions,num
+    label.config(text=db_questions[que_num])
+
+
+
 def get_options(que_num):
-    global db_options,rb
-    option1.config(text=db_options[que_num][0],value=1,variable=)
-    option2.config(text=db_options[que_num][1],value=2,variable)
-    option3.config(text=db_options[que_num][2],value=3,variable=rb)
-    option4.config(text=db_options[que_num][3],value=4,variable=rb)
+    global db_options
+    option1.config(text=db_options[que_num][0],value=1,variable=opt_selected)
+    option2.config(text=db_options[que_num][1],value=2,variable=opt_selected)
+    option3.config(text=db_options[que_num][2],value=3,variable=opt_selected)
+    option4.config(text=db_options[que_num][3],value=4,variable=opt_selected)
     
 def check_ans(que_num):
-    global db_answers,score
-    if(rb.get()==db_answers[que_num]):
-        score+=4
-    print(score)
-        
+    s = 0
+    global db_answers
+    if(str(opt_selected.get())==db_answers[que_num]):
+        s += 4
+    print(s)
+
+
+   
 
 
 btn = Button(root,text="Next",font=("Code New Roman",14,"bold"),bg="green",fg="white",command=action_next)
 btn.place(x=275,y=350)
 
-label = Label(root,text=db_questions[num],font=("Code New Roman",14,"bold"))
+label = Label(root,text=db_questions[0],font=("Code New Roman",14,"bold"))
 label.place(x=100,y=100)
 
-option1 = Radiobutton(root,text=db_options[num][0],value=1,variable=rb)
+option1 = Radiobutton(root,text=db_options[0][0],value=1,variable=opt_selected)
 option1.place(x=100,y=150)
 
-option2 = Radiobutton(root,text=db_options[num][1],value=2,variable=rb)
+option2 = Radiobutton(root,text=db_options[0][1],value=2,variable=opt_selected)
 option2.place(x=100,y=175)
 
-option3 = Radiobutton(root,text=db_options[num][2],value=3,variable=rb)
+option3 = Radiobutton(root,text=db_options[0][2],value=3,variable=opt_selected)
 option3.place(x=100,y=200)
 
-option4 = Radiobutton(root,text=db_options[num][3],value=4,variable=rb)
+option4 = Radiobutton(root,text=db_options[0][3],value=4,variable=opt_selected)
 option4.place(x=100,y=225)
 
 root.mainloop()
